e3f609c7b9d0c8e906fcc2d149916af77dfd0478
witpinegit <353565416@qq.com>
2019-12-19 18:20:59 +0800

    组件间传值方式1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,35 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    proList: [{
+        name: "james",
+        no: 23,
+        team: "湖人队",
+        salary: "4000万"
+      },
+      {
+        name: "kobe",
+        no: 24,
+        team: "湖人队",
+        salary: "3500万"
+      },
+      {
+        name: "curry",
+        no: 30,
+        team: "勇士队",
+        salary: "4300万"
+      },
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
+  toDetail: function(e) {
+    var index = e.currentTarget.dataset.index; //得到点击的是哪一条的index
+    //把被点击的这一条的详细信息显示到详情页面
+    var name = this.data.proList[index].name;
+    var no = this.data.proList[index].no;
+    var team = this.data.proList[index].team;
+    var salary = this.data.proList[index].salary;
     wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      url: '/pages/detail/detail?name=' + name + '&no=' + no + '&team=' + team + '&salary=' + salary
     })
   }
 })
